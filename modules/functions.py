4fb0989aab21933e860d1d47e215192be11a8528
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2019-04-08 09:27:08 +0200

    Reformat

--- modules/functions.py
+++ modules/functions.py
@@ -10,117 +10,113 @@ import os
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-###################################################################
 
 def call_skyview(field, survey, pos, fov, coord, proj='Car', pix=500):
-	"""Call Skyview to download data from a survey based on input parameters
+    """Call Skyview to download data from a survey based on input parameters
 
-	Args:
-		field (str): name of the field, used in naming the output file
-		survey (str): name of survey, from https://skyview.gsfc.nasa.gov/current/cgi/survey.pl
-		pos (float,float): position coordinates as a tuple
-		fov (float): FOV in degrees
-		coord (str): coordinate system (e.g. Galactic, J2000, B1950)
-		proj (str): projection of image. (e.g. Car, Sin)
-		pix (int): pixel dimensions of image (e.g. 500)
+    Args:
+        field (str): name of the field, used in naming the output file
+        survey (str): name of survey, from https://skyview.gsfc.nasa.gov/current/cgi/survey.pl
+        pos (float,float): position coordinates as a tuple
+        fov (float): FOV in degrees
+        coord (str): coordinate system (e.g. Galactic, J2000, B1950)
+        proj (str): projection of image. (e.g. Car, Sin)
+        pix (int): pixel dimensions of image (e.g. 500)
 
-	Returns:
-		str: name of resulting fits file
+    Returns:
+        str: name of resulting fits file
 
-	Examples:
-		>>> call_skyview('pks1657-298', 'dss', (255.291,-29.911), 5, 'J2000')
-		'skyview_pks1657-298_dss.fits'
-		>>> call_skyview('B0329+54', 'nvss', (144.99497,-01.22029), 0.5, 'Gal')
-		'skyview_B0329+54_nvss.fits'
-	"""
+    Examples:
+        >>> call_skyview('pks1657-298', 'dss', (255.291,-29.911), 5, 'J2000')
+        'skyview_pks1657-298_dss.fits'
+        >>> call_skyview('B0329+54', 'nvss', (144.99497,-01.22029), 0.5, 'Gal')
+        'skyview_B0329+54_nvss.fits'
+    """
 
-	# Assign position coordinates
-	x,y = pos[0],pos[1]
+    # Assign position coordinates
+    x, y = pos[0], pos[1]
 
-	# Construct name of the resulting file
-	fitsname = "Skyview_{field}_{survey}.fits".format(**locals())
+    # Construct name of the resulting file
+    fitsname = "Skyview_{field}_{survey}.fits".format(**locals())
 
-	# Construct and send the command
-	cmd = 'java -jar skyview.jar coordinates={coord} projection={proj} position={x:.4f},{y:.4f} size={fov},{fov} pixels={pix},{pix} survey="{survey}" output="results/{fitsname}"'.format(**locals())
-	print(cmd)
-	print('Executing command...')
-	os.system(cmd)
-	print('... done!')
+    # Construct and send the command
+    cmd = 'java -jar skyview.jar coordinates={coord} projection={proj} position={x:.4f},{y:.4f} size={fov},{fov} pixels={pix},{pix} survey="{survey}" output="results/{fitsname}"'.format(**locals())
+    print(cmd)
+    print('Executing command...')
+    os.system(cmd)
+    print('... done!')
 
-	return(fitsname)
+    return (fitsname)
 
-###################################################################
 
 def upload_to_google(img_path):
-	"""Upload a file using Google API to Google Drive folder
+    """Upload a file using Google API to Google Drive folder
 
-	Args:
-		filename (str): name of file to upload, including path
+    Args:
+        filename (str): name of file to upload, including path
 
-	Returns:
-		image_id (str): Google Drive image ID
+    Returns:
+        image_id (str): Google Drive image ID
 
-	Examples:
-		>>> upload_to_google("test.jpg")
-	"""
+    Examples:
+        >>> upload_to_google("test.jpg")
+    """
 
-	# Upload the resulting image to Google Drive
-	# This connects to a specific account made for this exercise
+    # Upload the resulting image to Google Drive
+    # This connects to a specific account made for this exercise
 
-	# This is a shared folder so automatically gives uploaded files read/write permission
-	folder_id = "1OuvohOT1aBpYBLToG5eIJbfdW-Z5a8Nj"
+    # This is a shared folder so automatically gives uploaded files read/write permission
+    folder_id = "1OuvohOT1aBpYBLToG5eIJbfdW-Z5a8Nj"
 
-	# Set up authorisation
-	gauth = GoogleAuth()
-	gauth.LocalWebserverAuth() 
-	drive = GoogleDrive(gauth)
+    # Set up authorisation
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
 
-	with open(img_path,"r") as img_file:
-		file_drive = drive.CreateFile({'title':os.path.basename(img_file.name), "parents": [{"kind": "drive#fileLink","id": folder_id}]})  
-		file_drive.SetContentFile(img_path) 
-		file_drive.Upload()
+    with open(img_path, "r") as img_file:
+        file_drive = drive.CreateFile({'title': os.path.basename(img_file.name), "parents": [{"kind": "drive#fileLink", "id": folder_id}]})
+        file_drive.SetContentFile(img_path)
+        file_drive.Upload()
 
-	# This part returns the Google Drive ID of the file
-	# We need this for the Slack upload
-	return file_drive['id']
+    # This part returns the Google Drive ID of the file
+    # We need this for the Slack upload
+    return file_drive['id']
 
-###################################################################
 
 def send_to_slack(msg_color, msg_text, field, slack_id, image_id):
+    """Send a post to Slack using Slack webhooks
 
-	"""Send a post to Slack using Slack webhooks
+    Args:
+        msg_color (str): any of 'good','warning', 'danger' or a HEX color
+        msg_text (str): text of choice to accompany the post
+        field (str): name of the field shown in the image posted
+        slack_id (str): poster's Slack ID e.g. 'UH0H2QFC2'
+        image_id (str): id of chosen image from Google Drive upload
 
-	Args:
-		msg_color (str): any of 'good','warning', 'danger' or a HEX color
-		msg_text (str): text of choice to accompany the post
-		field (str): name of the field shown in the image posted
-		slack_id (str): poster's Slack ID e.g. 'UH0H2QFC2'
-		image_id (str): id of chosen image from Google Drive upload
+    Returns:
+        None
 
-	Returns:
-		None
+    Examples:
+        >>> send_to_slack('#3A143E', 'Test', 'Test', 'UH0H2QFC2', '1qWyC6xAHODREDfoZLH4qTYTDwt5m3EEk')
+    """
 
-	Examples:
-		>>> send_to_slack('#3A143E', 'Test', 'Test', 'UH0H2QFC2', '1qWyC6xAHODREDfoZLH4qTYTDwt5m3EEk')
-	"""
+    # Replace characters in message text
+    msg_text = msg_text.replace("'", "")
 
-	# Replace characters in message text
-	msg_text = msg_text.replace("'","")
-
-	# Construct the full message
-	full_msg = """{
-	"attachments": [
-		{
-			"color": "%s",
-			"author_name": "<@%s>",
-			"title": "SkyviewBot Image Post: %s",
-			"text": "%s",
-			"image_url" : "http://drive.google.com/uc?export=download&id=%s"
-	   }
-	]
+    # Construct the full message
+    full_msg = """{
+    "attachments": [
+        {
+            "color": "%s",
+            "author_name": "<@%s>",
+            "title": "SkyviewBot Image Post: %s",
+            "text": "%s",
+            "image_url" : "http://drive.google.com/uc?export=download&id=%s"
+       }
+    ]
 }""" % (msg_color, slack_id, field, msg_text, image_id)
-	
-	# Send the command
-	cmd = """curl -X POST --data-urlencode 'payload=%s' https://hooks.slack.com/services/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""" % (full_msg)
-	print(cmd)
-	os.system(cmd)
+
+    # Send the command
+    cmd = """curl -X POST --data-urlencode 'payload=%s' https://hooks.slack.com/services/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""" % (full_msg)
+    print(cmd)
+    os.system(cmd)
--- skyviewbot.py
+++ skyviewbot.py
@@ -6,8 +6,6 @@ __author__ = "YOUR NAME HERE"
 __date__ = "$08-apr-2019 12:00:00$"
 __version__ = "0.1"
 
-import os
-import sys
 from modules.functions import *
 from argparse import ArgumentParser, RawTextHelpFormatter
 import aplpy
@@ -22,131 +20,119 @@ mpl.rcParams['ytick.direction'] = 'in'
 mpl.rcParams['xtick.top'] = True
 mpl.rcParams['ytick.right'] = True
 
-###################################################################
 
-def skyviewbot():
+def skyviewbot(*args):
+    """Command-line interface to skyviewbot"""
 
-	# Set up argument parsing
+    # Make sure you use meaningful variable names!
+    # Arguments should include:
+    # - option to download from Skyview or use existing FITS file (included)
+    # - custom field name
+    # - survey to download from Skyview
+    # - position to centre the field on
+    # - field of view in degrees
+    # - coordinate system for the image
+    # - output image name to save as a JPEG
+    # - colormap to use for imaging (optional!)
+    # - option to include a colorbar or not (optional!)
+    # - Slack ID of the user making the post
+    # - custom colour to post to Slack (optional!)
+    # - text to accompany the image posted to Slack (optional!)
 
-	# Make sure you use meaningful variable names! 
-	# Arguments should include:
-	# - option to download from Skyview or use existing FITS file (included)
-	# - custom field name
-	# - survey to download from Skyview
-	# - position to centre the field on
-	# - field of view in degrees
-	# - coordinate system for the image
-	# - output image name to save as a JPEG
-	# - colormap to use for imaging (optional!)
-	# - option to include a colorbar or not (optional!)
-	# - Slack ID of the user making the post
-	# - custom colour to post to Slack (optional!)
-	# - text to accompany the image posted to Slack (optional!)
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+    # ARGUMENTS HERE!!
+    parser.add_argument('-s', '--skyview',
+                        default=False,
+                        action='store_true',
+                        help='Specify whether to download a region from Skyview (default: %(default)s)')
+    parser.add_argument('-f', '--fits_name',
+                        default=None,
+                        type=str,
+                        help='Specify name of a custom fits file to use as input (default: %(default)s)')
+    args = parser.parse_args()
 
-	parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
-	### ARGUMENTS HERE!! ###
-	parser.add_argument('-s', '--skyview',
-						default=False,
-						action = 'store_true',
-						help='Specify whether to download a region from Skyview (default: %(default)s)')
-	parser.add_argument('-f', '--fits_name',
-						default=None,
-						type=str,
-						help='Specify name of a custom fits file to use as input (default: %(default)s)')		
-	args = parser.parse_args()
+    # Download an image of choice or use existing one
 
-	###################################################################
+    # This section should be able to handle querying Skyview, using a custom FITS or the included one
+    # Some possible ways to improve:
+    # - maybe the region cutout has different width and height dimensions?
+    # - maybe the user wants to select on wavelength type, e.g. radio, optical, etc
+    # - what if the FITS file of choice is online somewhere, to be downloaded?
+    # - what if the user can't get Java working, can you provide an astroquery.Skyview alternative?
 
-	# Download an image of choice or use existing one
+    if args.skyview:
+        # All parameters in this should be set properly using argparse
+        # e.g. call_skyview(args.field, args.survey, args.pos, args.fov, args.coord)
+        fits_name = call_skyview('PKS1657-298', 'DSS', (255.291, -29.911), 1, 'J2000')
+    elif args.fits_name:
+        fits_name = args.fits_name
+    else:
+        fits_name = 'results/Skyview_PKS1657-298_DSS.fits'
 
-	# This section should be able to handle querying Skyview, using a custom FITS or the included one
-	# Some possible ways to improve:
-	# - maybe the region cutout has different width and height dimensions?
-	# - maybe the user wants to select on wavelength type, e.g. radio, optical, etc
-	# - what if the FITS file of choice is online somewhere, to be downloaded? 
-	# - what if the user can't get Java working, can you provide an astroquery.Skyview alternative?
+    # This shouldn't be hardcoded, but set as an input argument in the parser above
+    field = 'PKS1657-298'
 
-	if args.skyview:
-		# All parameters in this should be set properly using argparse
-		# e.g. call_skyview(args.field, args.survey, args.pos, args.fov, args.coord)
-		fits_name = call_skyview('PKS1657-298','DSS',(255.291,-29.911),1,'J2000')
-	elif args.fits_name:
-		fits_name = args.fits_name
-	else: 
-		fits_name = 'results/Skyview_PKS1657-298_DSS.fits'
-		
-	# This shouldn't be hardcoded, but set as an input argument in the parser above
-	field = 'PKS1657-298'
+    # Make an image using aplpy
 
-	###################################################################
+    # Modify the below to be a function call in modules/functions.py
+    # Make sure you include docstrings with your function to explain what it does!
+    # Use the examples in functions.py to guide your docstring
+    # With the following as options (add more if you want to):
+    # - fits_name (file name of FITS image)
+    # - colormap name
+    # - optional to include a colorbar or not
+    # - img_name (name of the produced output file)
 
-	# Make an image using aplpy
+    # Current parameters
+    cmap_name = 'viridis'
+    colorbar = True  # As an Aussie, I really struggle with American spelling in Python
+    img_name = 'This_should_be_a_better_filename.jpg'
 
-	# Modify the below to be a function call in modules/functions.py
-	# Make sure you include docstrings with your function to explain what it does!
-	# Use the examples in functions.py to guide your docstring
-	# With the following as options (add more if you want to):
-	# - fits_name (file name of FITS image)
-	# - colormap name
-	# - optional to include a colorbar or not
-	# - img_name (name of the produced output file)
+    # Construct the figure
+    f = aplpy.FITSFigure(fits_name, figsize=(10, 8))
+    plt.title(field)
+    f.show_colorscale(cmap=cmap_name, stretch='linear')
+    f.ticks.set_color('k')
+    if colorbar:
+        f.add_colorbar()
 
-	# Current parameters
-	cmap_name = 'viridis' 
-	colorbar = True # As an Aussie, I really struggle with American spelling in Python
-	img_name = 'This_should_be_a_better_filename.jpg'
+    # Note: bbox_inches='tight' gets rid of annoying white space, very useful!
+    plt.savefig('results/' + img_name, dpi=200, bbox_inches='tight')
 
-	# Construct the figure
-	f = aplpy.FITSFigure(fits_name,figsize=(10,8))
-	plt.title(field)
-	f.show_colorscale(cmap=cmap_name,stretch='linear')
-	f.ticks.set_color('k')
-	if colorbar:
-		f.add_colorbar()
+    # Upload the image to Google/Dropbox
 
-	# Note: bbox_inches='tight' gets rid of annoying white space, very useful!
-	plt.savefig('results/'+img_name,dpi=200,bbox_inches='tight')
+    # This is done using a pre-written function included in modules/functions.py
+    # Note: you need to login when prompted in the browser
+    # With the autoskyview@gmail.com address, not your own!!!
+    # See slides for login information
+    # Possible way to improve:
+    # - what if the user doesn't want to save everything to "results/"?
+    # - what happens if something goes wrong with the image upload?
 
-	###################################################################
+    img_path = 'results/' + img_name
+    image_id = upload_to_google(img_path)
 
-	# Upload the image to Google/Dropbox
+    # Send the results to Slack
 
-	# This is done using a pre-written function included in modules/functions.py
-	# Note: you need to login when prompted in the browser 
-	# With the autoskyview@gmail.com address, not your own!!! 
-	# See slides for login information
-	# Possible way to improve:
-	# - what if the user doesn't want to save everything to "results/"?
-	# - what happens if something goes wrong with the image upload? 
+    # Modify the below to have these parameters set by your argparse arguments
+    # Specifically (add more options if you want to):
+    # - msg_color: colour of the message side
+    # - msg_text: text to accompany the post
+    # - field: name of the field in your image
+    # - slack_id: your Slack ID
+    # Note: if you add more options, you need to modify also send_to_slack()
 
-	img_path = 'results/'+img_name
-	image_id = upload_to_google(img_path)
+    msg_color = '#3D99DD'  # Little known fact: this colour is known as Celestial Blue
+    msg_text = 'PKS1657-298 is a great galaxy, maybe the best galaxy.'  # 1707.01542
+    slack_id = None  # This should be your own Slack ID, if you're testing the code
 
-	###################################################################
+    # Check for Slack ID
+    if not slack_id:
+        send_to_slack(msg_color, msg_text, field, slack_id, image_id)
+    else:
+        print('You should change the ??? to be your Slack ID before posting! Exiting...')
+        return
 
-	# Send the results to Slack
 
-	# Modify the below to have these parameters set by your argparse arguments
-	# Specifically (add more options if you want to): 
-	# - msg_color: colour of the message side 
-	# - msg_text: text to accompany the post
-	# - field: name of the field in your image
-	# - slack_id: your Slack ID
-	# Note: if you add more options, you need to modify also send_to_slack()
-
-	msg_color = '#3D99DD' # Little known fact: this colour is known as Celestial Blue 
-	msg_text = 'PKS1657-298 is a great galaxy, maybe the best galaxy.' # 1707.01542
-	slack_id = '???' # This should be your own Slack ID, if you're testing the code
-
-	# Check for Slack ID
-	if slack_id != '???':
-		send_to_slack(msg_color, msg_text, field, slack_id, image_id)
-	else:
-		print('You should change the ??? to be your Slack ID before posting! Exiting...')
-		sys.exit()
-
-###################################################################
-
-# This part allows you to run the script as normal, as well as a function form
 if __name__ == '__main__':
-    skyviewbot()
+    skyviewbot(sys.argv)
